7ee82ecd7d5cd3afa9758b9c287d43d7acb61bed
Joymar Ricardo Pinto Ortiz <joymargd2.2@gmail.com>
2026-09-08 16:03:04 -0500

    avance v5

--- src/server.js
+++ src/server.js
@@ -2,14 +2,14 @@ require("dotenv").config();
 
 const app = require("./app");
 const connectDB = require("./config/database");
-const path = require("path");
+
 const express= require("express")
 
 const PORT = process.env.PORT || 3000;
 
 connectDB();
 
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static("public"));
 
 app.listen(PORT, () => {
     console.log(`Servidor ejecutándose en el puerto ${PORT}`);
